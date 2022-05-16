6af825208f1f4d9e3274b078a0b2dc77bfd64aad
biplabiswas1234 <biplabiswas1234@gmail.com>
2022-05-16 21:00:46 +0530

    UC2[Biplab] Add: Finding largest line

--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -6,8 +6,27 @@ namespace LineComparison
     {
         static void Main(string[] args)
         {
-          LenghtOfLine lenghtOfLine = new LenghtOfLine();
-            lenghtOfLine.LenghtLine();
+            LenghtOfLine obj1 = new LenghtOfLine();
+            double Lenght1 = obj1.LenghtLine();
+            double Lenght2 = obj1.LenghtLine();
+
+            Console.WriteLine("The lenght of the two points of line-1: " + Lenght1);
+            Console.WriteLine("The lenght of the two points of line-2: " + Lenght2);
+            if (Lenght1 < Lenght2)
+            {
+                Console.WriteLine("Line-1 is less than line-2");
+            }
+            else
+            {
+                if (Lenght2 < Lenght1)
+                {
+                    Console.WriteLine("Line-1 is Greater than line-2");
+                }
+                else
+                {
+                    Console.WriteLine("Line-1 is equal to line-2");
+                }
+            }
         }
     }
 }

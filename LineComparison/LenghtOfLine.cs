1a43b9b33c566ab2957e3afa41b9ac0b0d4c45b2
biplabiswas1234 <biplabiswas1234@gmail.com>
2022-05-16 21:19:25 +0530

    UC3[Biplab] Add: Check compare to another line

--- LineComparison/LenghtOfLine.cs
+++ LineComparison/LenghtOfLine.cs
@@ -9,6 +9,7 @@ namespace LineComparison
 
         public double LenghtLine()
         {
+
             Console.WriteLine("Enter the cartesian coordinates of  (x1,y1)");
             double x1 = Convert.ToDouble(Console.ReadLine());
             double y1 = Convert.ToDouble(Console.ReadLine());
